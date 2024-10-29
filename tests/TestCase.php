1acfe3c244d469c5db5d8c3129dda69177b1d0fc
Justin Lyons <justin.lyons@interworks.com>
2024-10-29 15:55:44 -0500

    Added config values and mock for init test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,17 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
+    protected function defineEnvironment($app)
+    {
+        // Setup default database to use sqlite :memory:
+        tap($app['config'], function (Repository $config) {
+            $config->set('thoughtspotrest.auth', 'basic');
+            $config->set('thoughtspotrest.password', 'password');
+            $config->set('thoughtspotrest.url', 'https://example.thoughtspot.com');
+            $config->set('thoughtspotrest.username', 'username');
+        });
+    }
+
     protected function getPackageProviders($app)
     {
         return [
--- tests/Unit/ThoughtSpotRESTTest.php
+++ tests/Unit/ThoughtSpotRESTTest.php
@@ -1,8 +1,15 @@
 <?php
 
+use Mockery;
 use InterWorks\ThoughtSpotREST\ThoughtSpotREST;
 
 test('can be initialized', function () {
+    // Set up mock
+    $mock = Mockery::mock(ThoughtSpotREST::class)->makePartial();
+    $mock->shouldReceive('call')
+        ->withArgs(['auth/token/full', ['username' => 'username', 'password' => 'password'], 'POST'])
+        ->andReturn(['token' => 'fake_token']);
+
     $ts = new ThoughtSpotREST();
     expect($ts)->toBeInstanceOf(ThoughtSpotREST::class);
 });
