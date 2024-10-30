06c9f0587e250a47b4779fd1a373e180b0f42507
Justin Lyons <justin.lyons@interworks.com>
2024-10-30 10:47:42 -0500

    Mocked _authenticate function

--- tests/Unit/AuthenticationTest.php
+++ tests/Unit/AuthenticationTest.php
@@ -7,6 +7,11 @@ test('can get current user info', function () {
     // Create a mock for the class that uses the Authentication trait
     $mock = Mockery::mock(ThoughtSpotREST::class)->makePartial()->shouldAllowMockingProtectedMethods();
 
+    // Mock the _authenticate method
+    $mock->shouldReceive('_authenticate')
+        ->once()
+        ->andReturnNull();
+
     // Call the constructor explicitly to initialize properties
     $mock->__construct();
 
